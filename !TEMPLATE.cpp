77af63511482a5b2f0edcd10788530a237f50436
kamfora <kamil.forysiak@gmail.com>
2022-06-28 22:47:42 +0200

    poprawiam

--- !TEMPLATE.cpp
+++ !TEMPLATE.cpp
@@ -4,9 +4,9 @@ using namespace std;
 
 int main()
 {
+    return 0;
 
     cout << "koniec testu commitow";
 cout << "porpawka z poziomu strony";
 cout << "trzecia poprawka z poziomu vsc";
-    return 0;
 }
